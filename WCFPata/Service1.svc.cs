7a9b0e311e64e42c4febb8e10e7724f4bc86ca2d
jmno <crbikers@gmail.com>
2014-12-23 01:28:30 +0000

    Error1

--- WCFPata/Service1.svc.cs
+++ WCFPata/Service1.svc.cs
@@ -35,19 +35,7 @@ namespace WCFPata
             return string.Format("You entered: {0}", value);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-
+       
         private class Token
         {
             private string value;
