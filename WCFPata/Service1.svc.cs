668c08528eca41e494fde0f517f8883a0e4c1f2c
nelson <ntn_yo@hotmail.com>
2014-12-30 18:50:50 +0000

    novo teste log

--- WCFPata/Service1.svc.cs
+++ WCFPata/Service1.svc.cs
@@ -9,6 +9,7 @@ using System.Globalization;
 using System.IO;
 using System.Web.Hosting;
 using System.Diagnostics;
+using System.Web.Management;
 
 namespace WCFPata
 {
@@ -160,9 +161,8 @@ namespace WCFPata
             Token tokenObject;
             if (String.IsNullOrEmpty(token))
             {
+                new LogEvent("Error Is Null or Empty").Raise();
                 throw new ArgumentException("Error: invalid token value.");
-               
-
             }
             try
             {
@@ -549,7 +549,15 @@ namespace WCFPata
         }
 
 
+        public class LogEvent : WebRequestErrorEvent
+        {
+            public LogEvent(string message)
+                : base(null, null, 100001, new Exception(message))
+            {
+            }
+        }
     }
+
 
 
 }
