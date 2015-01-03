14eb204e9b71eeb16e145b6d54249ed90b3162c2
nelson <ntn_yo@hotmail.com>
2015-01-03 16:07:46 +0000

    teste 2 addterapeuta

--- WCFPata/Service1.svc.cs
+++ WCFPata/Service1.svc.cs
@@ -525,7 +525,7 @@ namespace WCFPata
             if (idConta != -1)
             {
                 Terapeuta t = new Terapeuta();
-                t.Conta.Id = idConta;
+                t.Conta = handler.getContaByID(idConta);
                 t.nome = terapeuta.nome;
                 t.cc = terapeuta.cc;
                 t.dataNasc = getData(terapeuta.dataNasc);
